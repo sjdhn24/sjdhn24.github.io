b4873722cbe89c9139f82dabe05a2909af413ac2
sjdhn24 <sjdhn24@gmail.com>
2017-08-27 21:44:58 -0400

    new changes

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('abc')
+
+const app = express()
+
+app.use('/static/', express.static('web'))
+
+app.get('/', function(req, res) {
+  res.send('Hello World ' + req.query.a)
+})
+
+app.get('/aboutme/:name', function(req, res) {
+  res.send('I am ' + req.params.name)
+})
+
+app.get('/db', function (req, res) {
+  db.all('SELECT rowid, info FROM lorem', function (err, data) {
+    res.send(data);
+  })
+})
+
+app.listen(5000, function () {
+  console.log('Server is running yoyo buyakasha')
+})
